fb3c2d1021b4ce83b821db9657f5402f1dcb876f
suns <suns@firsov.net>
2022-11-27 17:55:57 -0800

    2nd working draft * refactor sources, logic is intact * Chrome, FireFox, Edge compatibility

--- custom-element.js
+++ custom-element.js
@@ -1,74 +1,78 @@
-const XML_DECLARATION = '<?xml version="1.0" encoding="UTF-8"?>';
+const XML_DECLARATION = '<?xml version="1.0" encoding="UTF-8"?>'
+, XSL_NS_URL = 'http://www.w3.org/1999/XSL/Transform';
 
-export const log = { string: s=>console.debug(s), xml: x=>console.debug(x, {xml:new XMLSerializer().serializeToString(x)}) };
-
+export const log =
+{   string: s => console.debug( s ),
+    xml: x => console.debug( x, { xml: new XMLSerializer().serializeToString( x ) } )
+};
+const create = ( tag, t='' ) =>
+{
+    const e = document.createElement( tag );
+    if(t) e.innerText = t;
+    return e;
+}
 function xml2dom( xmlString )
 {
-    log.string({xmlString});
-    return new DOMParser().parseFromString( XML_DECLARATION+xmlString, "application/xml" )
+    return new DOMParser().parseFromString( XML_DECLARATION + xmlString, "application/xml" )
 }
+function bodyXml( dce )
+{   const s = new XMLSerializer().serializeToString( dce );
+    return s.substring( s.indexOf( '>' ) + 1, s.lastIndexOf( '<' ) );
+}
+function slot2xsl( s )
+{   const v = document.createElementNS( XSL_NS_URL, 'value-of' );
+    v.setAttribute( 'select', `//*[@slot="${ s.name }"]` );
+    s.parentNode.replaceChild( v, s );
+}
+function injectData( root, sectionName, arr, cb  )
+{
+    const create = tag => document.createElement( tag );
+    const inject = ( tag, parent, s ) =>
+                {
+                    parent.append( s = create( tag ) );
+                    return s;
+                };
+    const l = inject( sectionName, root );
+    [ ...arr ].forEach( e => l.append( cb(e) ) );
+}
+
 class CustomElement extends HTMLElement
 {
     constructor()
     {   super();
-        const tag = this.getAttribute('tag');
-        [...this.getElementsByTagName('slot')].forEach( s=>
-        {   const v= document.createElementNS("http://www.w3.org/1999/XSL/Transform",'value-of');
-            v.setAttribute('select',`/*/payload/*[@slot="${s.name}"]`);
-            s.parentNode.replaceChild(v,s);
-        });
-        const p = new XSLTProcessor();
-        const body = new XMLSerializer().serializeToString(this);
-        let inset = body.substring( body.indexOf('>')+1, body.lastIndexOf('<') );
-        p.importStylesheet(xml2dom( `<xsl:stylesheet version="1.0"
-                   xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 
+        [ ...this.getElementsByTagName( 'slot' ) ].forEach( slot2xsl );
+        const p = new XSLTProcessor();
+        p.importStylesheet( xml2dom( `<xsl:stylesheet version="1.0"
+    xmlns:xsl="${XSL_NS_URL}">
   <xsl:output method="html" />
 
   <xsl:template match="/">
     <xsl:apply-templates select="//attributes"/>
   </xsl:template>
   <xsl:template match="attributes">
-    ${inset}
+    ${ bodyXml(this) }
   </xsl:template>
 
-  <xsl:template match="@*|node()">
-      <xsl:copy>
-        <xsl:apply-templates select="@*|node()"/>
-      </xsl:copy>
-  </xsl:template>
-</xsl:stylesheet>` ));
+</xsl:stylesheet>` ) );
 
         class CustomTag extends HTMLElement
         {
             constructor()
             {
                 super();
-                const create = tag=> document.createElement(tag);
-                const text = (tag,t)=>{
-                    const e = create(tag);
-                    e.innerText = t;
-                    return e;
-                }
-                const inject = (tag, parent,s) =>
-                {   parent.append(s = create(tag));
-                    return s;
-                };
-                const s = create('div');
-                const l = inject('payload',s);
-                [...this.childNodes].forEach( e=>l.append(e));
-                const a = inject('attributes',s);
-                [...this.attributes].forEach( e=>a.append(text(e.nodeName,e.value)));
-                const d = inject('dataset',s);
-                Object.keys(this.dataset).forEach( k=>d.append(text( k,this.dataset[k] )));
-
-                log.xml( s);
-                const fragment = p.transformToFragment(s, document);
-                this.innerHTML='';
-                this.appendChild(fragment);
+                const x = create( 'div' );
+                injectData( x, 'payload'    , this.childNodes, e=>e  );
+                injectData( x, 'attributes' , this.attributes, e=>create( e.nodeName, e.value ) );
+                injectData( x, 'dataset'    , Object.keys( this.dataset ), k=>create( k, this.dataset[ k ] )  );
+                const f = p.transformToFragment( x, document );
+                this.innerHTML = '';
+                [...f.childNodes].forEach(e=>this.appendChild(e));
             }
         }
-        window.customElements.define( tag, CustomTag);
+
+        window.customElements.define( this.getAttribute( 'tag' ), CustomTag );
     }
 }
-window.customElements.define( 'custom-element', CustomElement);
+
+window.customElements.define( 'custom-element', CustomElement );
