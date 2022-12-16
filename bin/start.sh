901331cf5686d404bff2ca386544aaaa6a91addf
suns <suns@firsov.net>
2022-12-16 00:52:18 -0800

    fix for sandbox

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,10 @@
-npm i @web/dev-server
+
+if [ x"${GIT_PROXY}" == "x" ]; then
+      echo "PC"
+      npm i --no-save @web/dev-server
+  else
+      echo "SANDBOX"
+      npm i @web/dev-server
+  fi
+
 web-dev-server --node-resolve
