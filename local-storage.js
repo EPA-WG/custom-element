05fc17f46b9cc276c5e35c005d00c0daef9c30fe
suns <suns@firsov.net>
2023-02-09 20:35:27 -0800

    local-storage 1st working draft. string value

--- local-storage.js
+++ local-storage.js
@@ -1,17 +1,32 @@
-export class LocalStorage extends HTMLElement
+const attr = (el, attr)=> el.getAttribute(attr);
+
+export class LocalStorageElement extends HTMLElement
 {
         constructor()
         {
             super();
-            const attr = name => this.getAttribute(name)
-            ,      key = attr('key');
-            this.addEventListener('load',()=>
-            {   this.dispatchEvent( new Event('loadend',    {  bubbles: true
-                                                            ,     data: localStorage.getItem( key() )
-                                                            ,    slice:attr('slice')
-                                                            ,   target: this
-                                                            }));
-            });
+            // const attr = name => this.getAttribute(name)
+            // ,      key = attr('key');
+            // this.addEventListener('transformation',()=>
+            // {   this.dispatchEvent( new Event('loadend',    {    bubbles: true
+            //                                                 , cancelable: true
+            //                                                 ,       data: JSON.parse( localStorage.getItem( key() ) )
+            //                                                 ,      slice: attr('slice')
+            //                                                 ,     target: this
+            //                                                 }));
+            // });
         }
+        sliceInit( data )
+        {   const v = localStorage.getItem( attr( this,'key' ) );
+            const zz = this.dispatchEvent( new CustomEvent('loadend',
+                {  bubbles: true
+                , cancelable: true
+                ,   detail: v.startsWith('{') ? JSON.parse( v ) : v
+                }));
 
+            return data || {}
+        }
 }
+
+window.customElements.define( 'local-storage', LocalStorageElement );
+export default LocalStorageElement;
