d07a0ee0872b6cec7c56d9ff4fca345fc441d553
Burton Smith <31320098+break-stuff@users.noreply.github.com>
2024-02-22 01:41:52 +0000

    add VS Code and JetBrains IDE support

---
+++ custom-elements-manifest.config.mjs
@@ -0,0 +1,12 @@
+import { customElementVsCodePlugin } from "custom-element-vs-code-integration";
+import { customElementJetBrainsPlugin } from "custom-element-jet-brains-integration";
+
+export default {
+    /** Globs to analyze */
+    globs: ['components/*.js'],
+    /** Provide custom plugins */
+    plugins: [
+        customElementVsCodePlugin({ cssFileName: null }),
+        customElementJetBrainsPlugin({ packageJson: true })
+    ],
+  }
